74b4d2127615452d1d5c52c423cae8c7f779e6d9
tedkmburu <44197881+tedkmburu@users.noreply.github.com>
2021-07-14 23:31:43 -0400

    reordered videos

--- video14.js
+++ video14.js
@@ -5,124 +5,84 @@ let charges = [];
 let chargeDiameter = 40;
 let chargeRadius = chargeDiameter / 2;
 
-let backgroundCanvas
-let foreGroundCanvas
+function setup() 
+{
+    createCanvas(innerWidth, innerHeight);
 
+    charges.push(new Charge(createVector(400, 400), +5))
 
-const background = canvas => {
-
-    canvas.setup = function()  // This function only runs once when the page first loads. 
-    {
-      canvas.createCanvas(innerWidth, innerHeight); // creates the <canvas> that everything runs on.
-      backgroundCanvas = canvas;
-    }
-  
-    canvas.draw = function() // this function runs every frame. Everything on the background canvas starts here.
-    {  
-        canvas.background("black");
-
-        let voltageAccuracy = 15; 
-
-        canvas.push()
-            for (let x = 0; x < innerWidth; x+= voltageAccuracy) 
-            {
-                for (let y = 0; y < innerHeight; y+= voltageAccuracy) 
-                {
-                    canvas.noStroke()
-
-                    let voltage = voltageAtPoint(canvas.createVector(x, y))
-                    let intensity = canvas.map(Math.abs(voltage), 0, 5475, 0, 255)
-
-                
-
-                    let red = 0;
-                    let blue = 0;
-                    let green = 0;
-                    let alpha = intensity * 10;
-
-                    if (voltage > 0) red = intensity;
-                    else if (voltage < 0) blue = intensity;
-
-                    canvas.fill(red, green, blue, alpha)
-                    canvas.rect(x, y, voltageAccuracy, voltageAccuracy)
-                }
-            }
-            canvas.pop()
-    }
-  
-    canvas.windowResized = function() // inbuilt p5 function. runs everytime the window is resized
-    {
-      canvas.resizeCanvas(innerWidth, innerHeight); // resizes the canvas to fit the new window size
-    }
+    deselectCharges()
 }
 
+function draw() 
+{
+    background("black");
 
+    displayGrid()
 
-const foreGround = canvas => {
-  
-    canvas.setup = function()  // This function only runs once when the page first loads. 
-    {
-        canvas.createCanvas(innerWidth, innerHeight); // creates the <canvas> that everything runs on.
+    let voltageAccuracy = 15; 
 
-        
-        foreGroundCanvas = canvas;
-        charges.push(new Charge(canvas.createVector(400, 400), +5))
-        deselectCharges()
-    }
-  
-  
-  
-    canvas.draw = function() // this function runs every frame. Everything on the foreground canvas starts here.
-    {  
-      canvas.clear(); // clears the canvas so that it's transparent
-
-      displayGrid()
-
-        for (let i = 0; i < charges.length; i++) 
+    push()
+        for (let x = 0; x < innerWidth; x+= voltageAccuracy) 
         {
-            charges[i].display();
+            for (let y = 0; y < innerHeight; y+= voltageAccuracy) 
+            {
+                noStroke()
+
+                let voltage = voltageAtPoint(createVector(x, y))
+                let intensity = map(Math.abs(voltage), 0, 5475, 0, 255)
+
+               
+
+                let red = 0;
+                let blue = 0;
+                let green = 0;
+                let alpha = intensity * 10;
+
+                if (voltage > 0) red = intensity;
+                else if (voltage < 0) blue = intensity;
+
+                fill(red, green, blue, alpha)
+                rect(x, y, voltageAccuracy, voltageAccuracy)
+            }
         }
+    pop()
+
+    for (let i = 0; i < charges.length; i++) 
+    {
+        charges[i].display();
     }
-  
-    canvas.mouseClicked = function() { whenMouseClicked(); } // inbuilt p5 function. runs everytime any mouse button is clicked
-    canvas.mouseDragged = function() { whenMouseDragged(); } // inbuilt p5 function. runs everytime the mosue is dragged (clicked down and moving)
-    canvas.doubleClicked = function() { whenDoubleClicked(); } // inbuilt p5 function. runs everytime the mouse is double clicked
-   
 }
 
 
-new p5(background); // creates the background instance of p5
-new p5(foreGround); // creates the foreground instance of p5
+
 
 
 function displayGrid() // displays background grid
 {
-    let canvas = foreGroundCanvas
-    canvas.push();
-        canvas.stroke("rgba(255,255,255,0.25)"); // gray color for the grid
+    push();
+    stroke("rgba(255,255,255,0.25)"); // gray color for the grid
         for (let x = 0; x <= innerWidth; x+= gridSize)
         {
-            canvas.line(x, 0, x, innerHeight);
+        line(x, 0, x, innerHeight);
         }
         for (let y = 0; y < innerHeight; y+= gridSize)
         {
-            canvas.line(0, y, innerWidth, y);
+        line(0, y, innerWidth, y);
         }
-    canvas.pop();
+    pop();
 }
 
 
 
 function netForceAtPoint(position) 
 {
-    let canvas = foreGroundCanvas;
-
-    let finalVector = canvas.createVector(0, 0);
+    let finalVector = createVector(0, 0);
 
     for (let i = 0; i < charges.length; i++) 
     {
         let charge = charges[i];
-        let chargePosition = canvas.createVector(charge.position.x, charge.position.y);
+        let chargePosition = createVector(charge.position.x, charge.position.y);
 
         //F = KQ / (r^2)
         let kq = charge.charge * k;
@@ -137,7 +97,7 @@ function netForceAtPoint(position)
         let forceX = force * Math.cos(theta);
         let forceY = force * Math.sin(theta);
 
-        let forceVectors = canvas.createVector(forceX, forceY).mult(-1);
+        let forceVectors = createVector(forceX, forceY).mult(-1);
 
         finalVector.add(forceVectors);
     }
@@ -163,28 +123,26 @@ function voltageAtPoint(point)
 }
 
 
-function whenMouseClicked()
+function mouseClicked()
 {
     deselectCharges();
     selectACharge()
 }
 
 
-function whenDoubleClicked()
+function doubleClicked()
 {
     deselectCharges();
 
     if (!selectACharge()) 
     {
-        let canvas = foreGroundCanvas;
-        let mousePosition = canvas.createVector(canvas.mouseX, canvas.mouseY);
+        let mousePosition = createVector(mouseX, mouseY);
         charges.push(new Charge(mousePosition, 5))
     }
 }
 
-function whenMouseDragged()
+function mouseDragged()
 {
-    let canvas = foreGroundCanvas;
     selectACharge();
 
     for (let i = 0; i < charges.length; i++) 
@@ -193,15 +151,15 @@ function whenMouseDragged()
         let onSlider = false;
 
         // rect(this.x - 75, this.y + 25, 150, 25);
-        if (canvas.mouseX > charge.position.x - 75 && canvas.mouseX < charge.position.x + 75 && canvas.mouseY > charge.position.y + 25 && canvas.mouseY < charge.position.y + 50) 
+        if (mouseX > charge.position.x - 75 && mouseX < charge.position.x + 75 && mouseY > charge.position.y + 25 && mouseY < charge.position.y + 50) 
         {
             onSlider = true;
         }
         if (charges[i].selected && !onSlider) 
         {
             deselectCharges()
-            charge.position.x = canvas.mouseX;
-            charge.position.y = canvas.mouseY; 
+            charge.position.x = mouseX;
+            charge.position.y = mouseY; 
             charge.selected = true; 
             charge.dragging = true; 
             return
@@ -231,11 +189,10 @@ function deselectCharges()
 
 function selectACharge()
 {
-    let canvas = foreGroundCanvas;
     for (let i = 0; i < charges.length; i++) 
     {
         let chargePosition = charges[i].position;
-        let mousePosition = canvas.createVector(canvas.mouseX, canvas.mouseY)
+        let mousePosition = createVector(mouseX, mouseY)
         let distance = p5.Vector.dist(chargePosition, mousePosition)
 
         if (distance < chargeRadius) 
@@ -260,8 +217,6 @@ class Charge
 {
     constructor (position, charge)
     {
-        let canvas = foreGroundCanvas;
-
         this.position = position;
         this.charge = charge || 0; 
 
@@ -270,25 +225,23 @@ class Charge
         this.selected = true;
         this.dragging = false; 
 
-        this.slider = canvas.createSlider(-5, 5, this.charge, 1);
+        this.slider = createSlider(-5, 5, this.charge, 1);
         this.slider.position(this.position.x - 75, this.position.y + 20);
         this.slider.style("visibility", "visible");
     }
 
     display()
     {
-        let canvas = foreGroundCanvas;
-
         this.charge = this.slider.value()
         if (this.selected) 
         {
-            canvas.stroke("white")  
+            stroke("white")  
             this.slider.position(this.position.x - 75, this.position.y + 20);
             this.slider.style("visibility", "visible");
         }
         else
         {
-            canvas.stroke(0);
+            stroke(0);
             this.slider.style("visibility", "hidden");
         }
 
@@ -299,17 +252,15 @@ class Charge
         else if (this.charge > 0) color = "red";
         else   color = "grey";
 
-        canvas.fill(color);
-        canvas.ellipse(this.position.x, this.position.y, chargeDiameter, chargeDiameter);
+        fill(color);
+        ellipse(this.position.x, this.position.y, chargeDiameter, chargeDiameter);
 
         // rect(this.x - 75, this.y + 25, 150, 25);
 
-        canvas.noStroke()
-        canvas.fill("white");
-        canvas.textSize(16);
+        noStroke()
+        fill("white");
+        textSize(16);
         let chargeToShow = (this.charge > 0) ?  ("+" + this.charge) : this.charge;
-        canvas.text(chargeToShow, this.position.x - 9, this.position.y + 7);
+        text(chargeToShow, this.position.x - 9, this.position.y + 7);
     }
 }
-
-
--- video15.js
+++ video15.js
@@ -5,84 +5,124 @@ let charges = [];
 let chargeDiameter = 40;
 let chargeRadius = chargeDiameter / 2;
 
-function setup() 
-{
-    createCanvas(innerWidth, innerHeight);
+let backgroundCanvas
+let foreGroundCanvas
 
-    charges.push(new Charge(createVector(400, 400), +5))
 
-    deselectCharges()
-}
+const background = canvas => {
 
-function draw() 
-{
-    background("black");
-
-    displayGrid()
-
-    let voltageAccuracy = 15; 
-
-    push()
-        for (let x = 0; x < innerWidth; x+= voltageAccuracy) 
-        {
-            for (let y = 0; y < innerHeight; y+= voltageAccuracy) 
-            {
-                noStroke()
-
-                let voltage = voltageAtPoint(createVector(x, y))
-                let intensity = map(Math.abs(voltage), 0, 5475, 0, 255)
-
-               
-
-                let red = 0;
-                let blue = 0;
-                let green = 0;
-                let alpha = intensity * 10;
-
-                if (voltage > 0) red = intensity;
-                else if (voltage < 0) blue = intensity;
-
-                fill(red, green, blue, alpha)
-                rect(x, y, voltageAccuracy, voltageAccuracy)
-            }
-        }
-    pop()
-
-    for (let i = 0; i < charges.length; i++) 
+    canvas.setup = function()  // This function only runs once when the page first loads. 
     {
-        charges[i].display();
+      canvas.createCanvas(innerWidth, innerHeight); // creates the <canvas> that everything runs on.
+      backgroundCanvas = canvas;
+    }
+  
+    canvas.draw = function() // this function runs every frame. Everything on the background canvas starts here.
+    {  
+        canvas.background("black");
+
+        let voltageAccuracy = 15; 
+
+        canvas.push()
+            for (let x = 0; x < innerWidth; x+= voltageAccuracy) 
+            {
+                for (let y = 0; y < innerHeight; y+= voltageAccuracy) 
+                {
+                    canvas.noStroke()
+
+                    let voltage = voltageAtPoint(canvas.createVector(x, y))
+                    let intensity = canvas.map(Math.abs(voltage), 0, 5475, 0, 255)
+
+                
+
+                    let red = 0;
+                    let blue = 0;
+                    let green = 0;
+                    let alpha = intensity * 10;
+
+                    if (voltage > 0) red = intensity;
+                    else if (voltage < 0) blue = intensity;
+
+                    canvas.fill(red, green, blue, alpha)
+                    canvas.rect(x, y, voltageAccuracy, voltageAccuracy)
+                }
+            }
+            canvas.pop()
+    }
+  
+    canvas.windowResized = function() // inbuilt p5 function. runs everytime the window is resized
+    {
+      canvas.resizeCanvas(innerWidth, innerHeight); // resizes the canvas to fit the new window size
     }
 }
 
 
 
+const foreGround = canvas => {
+  
+    canvas.setup = function()  // This function only runs once when the page first loads. 
+    {
+        canvas.createCanvas(innerWidth, innerHeight); // creates the <canvas> that everything runs on.
+
+        
+        foreGroundCanvas = canvas;
+        charges.push(new Charge(canvas.createVector(400, 400), +5))
+        deselectCharges()
+    }
+  
+  
+  
+    canvas.draw = function() // this function runs every frame. Everything on the foreground canvas starts here.
+    {  
+      canvas.clear(); // clears the canvas so that it's transparent
+
+      displayGrid()
+
+        for (let i = 0; i < charges.length; i++) 
+        {
+            charges[i].display();
+        }
+    }
+  
+    canvas.mouseClicked = function() { whenMouseClicked(); } // inbuilt p5 function. runs everytime any mouse button is clicked
+    canvas.mouseDragged = function() { whenMouseDragged(); } // inbuilt p5 function. runs everytime the mosue is dragged (clicked down and moving)
+    canvas.doubleClicked = function() { whenDoubleClicked(); } // inbuilt p5 function. runs everytime the mouse is double clicked
+   
+}
+
+
+new p5(background); // creates the background instance of p5
+new p5(foreGround); // creates the foreground instance of p5
 
 
 function displayGrid() // displays background grid
 {
-    push();
-    stroke("rgba(255,255,255,0.25)"); // gray color for the grid
+    let canvas = foreGroundCanvas
+    canvas.push();
+        canvas.stroke("rgba(255,255,255,0.25)"); // gray color for the grid
         for (let x = 0; x <= innerWidth; x+= gridSize)
         {
-        line(x, 0, x, innerHeight);
+            canvas.line(x, 0, x, innerHeight);
         }
         for (let y = 0; y < innerHeight; y+= gridSize)
         {
-        line(0, y, innerWidth, y);
+            canvas.line(0, y, innerWidth, y);
         }
-    pop();
+    canvas.pop();
 }
 
 
 
 function netForceAtPoint(position) 
 {
-    let finalVector = createVector(0, 0);
+    let canvas = foreGroundCanvas;
+
+    let finalVector = canvas.createVector(0, 0);
 
     for (let i = 0; i < charges.length; i++) 
     {
         let charge = charges[i];
-        let chargePosition = createVector(charge.position.x, charge.position.y);
+        let chargePosition = canvas.createVector(charge.position.x, charge.position.y);
 
         //F = KQ / (r^2)
         let kq = charge.charge * k;
@@ -97,7 +137,7 @@ function netForceAtPoint(position)
         let forceX = force * Math.cos(theta);
         let forceY = force * Math.sin(theta);
 
-        let forceVectors = createVector(forceX, forceY).mult(-1);
+        let forceVectors = canvas.createVector(forceX, forceY).mult(-1);
 
         finalVector.add(forceVectors);
     }
@@ -123,26 +163,28 @@ function voltageAtPoint(point)
 }
 
 
-function mouseClicked()
+function whenMouseClicked()
 {
     deselectCharges();
     selectACharge()
 }
 
 
-function doubleClicked()
+function whenDoubleClicked()
 {
     deselectCharges();
 
     if (!selectACharge()) 
     {
-        let mousePosition = createVector(mouseX, mouseY);
+        let canvas = foreGroundCanvas;
+        let mousePosition = canvas.createVector(canvas.mouseX, canvas.mouseY);
         charges.push(new Charge(mousePosition, 5))
     }
 }
 
-function mouseDragged()
+function whenMouseDragged()
 {
+    let canvas = foreGroundCanvas;
     selectACharge();
 
     for (let i = 0; i < charges.length; i++) 
@@ -151,15 +193,15 @@ function mouseDragged()
         let onSlider = false;
 
         // rect(this.x - 75, this.y + 25, 150, 25);
-        if (mouseX > charge.position.x - 75 && mouseX < charge.position.x + 75 && mouseY > charge.position.y + 25 && mouseY < charge.position.y + 50) 
+        if (canvas.mouseX > charge.position.x - 75 && canvas.mouseX < charge.position.x + 75 && canvas.mouseY > charge.position.y + 25 && canvas.mouseY < charge.position.y + 50) 
         {
             onSlider = true;
         }
         if (charges[i].selected && !onSlider) 
         {
             deselectCharges()
-            charge.position.x = mouseX;
-            charge.position.y = mouseY; 
+            charge.position.x = canvas.mouseX;
+            charge.position.y = canvas.mouseY; 
             charge.selected = true; 
             charge.dragging = true; 
             return
@@ -189,10 +231,11 @@ function deselectCharges()
 
 function selectACharge()
 {
+    let canvas = foreGroundCanvas;
     for (let i = 0; i < charges.length; i++) 
     {
         let chargePosition = charges[i].position;
-        let mousePosition = createVector(mouseX, mouseY)
+        let mousePosition = canvas.createVector(canvas.mouseX, canvas.mouseY)
         let distance = p5.Vector.dist(chargePosition, mousePosition)
 
         if (distance < chargeRadius) 
@@ -217,6 +260,8 @@ class Charge
 {
     constructor (position, charge)
     {
+        let canvas = foreGroundCanvas;
+
         this.position = position;
         this.charge = charge || 0; 
 
@@ -225,23 +270,25 @@ class Charge
         this.selected = true;
         this.dragging = false; 
 
-        this.slider = createSlider(-5, 5, this.charge, 1);
+        this.slider = canvas.createSlider(-5, 5, this.charge, 1);
         this.slider.position(this.position.x - 75, this.position.y + 20);
         this.slider.style("visibility", "visible");
     }
 
     display()
     {
+        let canvas = foreGroundCanvas;
+
         this.charge = this.slider.value()
         if (this.selected) 
         {
-            stroke("white")  
+            canvas.stroke("white")  
             this.slider.position(this.position.x - 75, this.position.y + 20);
             this.slider.style("visibility", "visible");
         }
         else
         {
-            stroke(0);
+            canvas.stroke(0);
             this.slider.style("visibility", "hidden");
         }
 
@@ -252,16 +299,16 @@ class Charge
         else if (this.charge > 0) color = "red";
         else   color = "grey";
 
-        fill(color);
-        ellipse(this.position.x, this.position.y, chargeDiameter, chargeDiameter);
+        canvas.fill(color);
+        canvas.ellipse(this.position.x, this.position.y, chargeDiameter, chargeDiameter);
 
         // rect(this.x - 75, this.y + 25, 150, 25);
 
-        noStroke()
-        fill("white");
-        textSize(16);
+        canvas.noStroke()
+        canvas.fill("white");
+        canvas.textSize(16);
         let chargeToShow = (this.charge > 0) ?  ("+" + this.charge) : this.charge;
-        text(chargeToShow, this.position.x - 9, this.position.y + 7);
+        canvas.text(chargeToShow, this.position.x - 9, this.position.y + 7);
     }
 }
 
